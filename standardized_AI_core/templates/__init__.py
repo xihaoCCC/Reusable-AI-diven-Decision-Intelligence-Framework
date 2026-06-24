4d515ed1fdab2fe6bf3235443c4acd42e8e5c363
Xihao Cao <xihaocao@163.com>
2026-06-23 21:33:15 -0500

    Remove the old dependencies

--- standardized_AI_core/templates/__init__.py
+++
@@ -1,5 +0,0 @@
-from standardized_AI_core.templates.deployment_template import (
-    build_deployment_template,
-)
-
-__all__ = ["build_deployment_template"]
--- standardized_AI_core/workflow/__init__.py
+++
@@ -1,3 +0,0 @@
-from standardized_AI_core.workflow.pipeline import DecisionIntelligencePipeline
-
-__all__ = ["DecisionIntelligencePipeline"]
